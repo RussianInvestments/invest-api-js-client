49bc892249b771d538f95f6a82ccc6cd57ea8960
s.radyukov <sradyukov@gmail.com>
2025-02-01 16:09:28 +0300

    grpc-lib-1

---
+++ grpc/src/index.ts
@@ -0,0 +1,12 @@
+export { TTechApiClient } from './client2'
+export * from './api/instruments'
+export * from './api/marketdata'
+export * from './api/orders'
+export * from './api/operations'
+export * from './api/stoporders'
+export * from './api/users'
+export * from './api/common'
+
+
+
+//esModuleInterop
